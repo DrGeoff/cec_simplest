d86a88f2afa16d3946a81ead25b5588c7eb55363
DrGeoff <drgeoffathome@gmail.com>
2015-08-01 19:11:24 +1000

    Make the build.sh just grab the build command out of the source code comment so that the two can't get out of sync. Make sure that -ldl is specified in the build command. Make sure g++-4.8 is used (again)

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -eu
 
-g++ -std=gnu++0x -fPIC -g -Wall -march=armv6 -mfpu=vfp -mfloat-abi=hard -isystem /opt/vc/include/ -isystem /opt/vc/include/interface/vcos/pthreads/ -isystem /opt/vc/include/interface/vmcs_host/linux/ -I/usr/local/include -L /opt/vc/lib -lcec -lbcm_host -ldl cec-simplest.cpp -o cec-simplest 
+# grab the g++ example line from the comment inside the source code
+build_cmd=$(grep 'g++' cec-simplest.cpp | cut -d' ' -f 2-)
 
+# build it
+eval $build_cmd
--- cec-simplest.cpp
+++ cec-simplest.cpp
@@ -1,6 +1,5 @@
 // Build command:
-// g++  -std=gnu++0x -fPIC -g -Wall -march=armv6 -mfpu=vfp -mfloat-abi=hard -isystem /opt/vc/include/ -isystem /opt/vc/include/interface/vcos/pthreads/ -isystem /opt/vc/include/interface/vmcs_host/linux/ -I/usr/local/include -L /opt/vc/lib -lcec -lbcm_host cec-simplest.cpp -o cec-simplest
-
+// g++-4.8 -std=gnu++0x -fPIC -g -Wall -march=armv6 -mfpu=vfp -mfloat-abi=hard -isystem /opt/vc/include/ -isystem /opt/vc/include/interface/vcos/pthreads/ -isystem /opt/vc/include/interface/vmcs_host/linux/ -I/usr/local/include -L /opt/vc/lib -lcec -lbcm_host -ldl cec-simplest.cpp -o cec-simplest 
 //#CXXFLAGS=-I/usr/local/include
 //#LINKFLAGS=-lcec -ldl
 #include <libcec/cec.h>
